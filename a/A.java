39e225da44c8a6d1e83887be751900471d1e3ccb
faran230 <130466482+faran230@users.noreply.github.com>
2025-04-22 18:47:09 +0200

    Übung a und b bearbeitet

--- a/A.java
+++ a/A.java
@@ -1,22 +1,26 @@
+
 package a;
 
 import java.util.Random;
 
 public class A {
-	public static void (String args) {
-		int s = "Hello World!";
-		char c = 'X';
-		int[] numberList = int[15];
-		for (int i; i < numberList.length; i++) {
-			numberList[i] = random.nextInt(100);
-		}
-		
-		sum = 0;
-		for (int i = 0; i < numberList.length; c++) {
-			sum += numberList[i];
-		}
-		if sum > 750 {
-			println(s);
-		}
-	}
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        char c = 'X';
+        int[] numberList = new int[15];
+        Random random = new Random();
+
+        for (int i = 0; i < numberList.length; i++) {
+            numberList[i] = random.nextInt(100);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < numberList.length; i++) {
+            sum += numberList[i];
+        }
+
+        if (sum > 750) {
+            System.out.println(s);
+        }
+    }
 }
--- b/ATM.java
+++ b/ATM.java
@@ -3,36 +3,48 @@ package b;
 import java.io.*;
 
 public class ATM {
-	
-	/**
-	 * Main command loop of the ATM
-	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
-	 * which actually does the calculation and produces money.
-	 * If the user enters anything else than an integer number, the loop breaks 
-	 * and the program exists
-	 */
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			} catch (Exception e) {
-				break;
-			}
-		}
-	}
-	
-	public void cashout(int amount) {
-		
-	};
-	
-	/**
-	 * Launches the ATM
-	 */
-	public static void main(String[] args) {
-		ATM atm = new ATM();
-		atm.run();
-	};
-};
+
+    private int balance = 100; 
+
+    /**
+     * Main command loop of the ATM
+     */
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("Enter the amount to withdraw: ");
+                String input = br.readLine();
+
+                // 👋 Programm beenden, wenn "exit" eingegeben wird
+                if (input.equalsIgnoreCase("exit")) {
+                    System.out.println("Goodbye!");
+                    break;
+                }
+
+                int amount = Integer.parseInt(input); // versuche, Zahl zu lesen
+
+                if (amount <= 0) {
+                    System.out.println("Please enter a positive amount.");
+                } else if (amount > balance) {
+                    System.out.println("Sorry, not enough money in the bank.");
+                } else {
+                    balance -= amount;
+                    System.out.println("Ok, here is your money, enjoy!");
+                }
+
+            } catch (NumberFormatException e) {
+                // Wenn keine ganze Zahl eingegeben wurde
+                System.out.println("Invalid input. Please enter a whole number or 'exit'.");
+            } catch (IOException e) {
+                System.out.println("Input error. Exiting...");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.run();
+    }
+}
