82552f430a5f78504f4683f5993b3973cf061104
lazzosh <lazzosh@Lazzo-Kln>
2025-04-23 15:32:31 +0200

    finished exercise final

--- a/A.java
+++ a/A.java
@@ -2,21 +2,26 @@ package a;
 
 import java.util.Random;
 
+// Comments explain the wrong code
+
 public class A {
-	public static void (String args) {
-		int s = "Hello World!";
-		char c = 'X';
-		int[] numberList = int[15];
-		for (int i; i < numberList.length; i++) {
+	public static void main(String[] args) { // wrong Syntax main-Methode 
+		String s = "Hello World!"; // wrong variable: String
+		char c = 'X'; // char unused ¯\_(ツ)_/¯
+		int[] numberList = new int[15]; // create 'new' list
+		Random random = new Random(); // create new Object
+		
+		
+		for (int i = 0; i < numberList.length; i++) { // interval wrongly defined
 			numberList[i] = random.nextInt(100);
 		}
 		
-		sum = 0;
-		for (int i = 0; i < numberList.length; c++) {
+		int sum = 0; // missing variable
+		for (int i = 0; i < numberList.length; i++) { // interval wrongly defined
 			sum += numberList[i];
 		}
-		if sum > 750 {
-			println(s);
+		if (sum > 750) { //wrong Syntax: missing brackets
+			System.out.println(s); // wrong print method
 		}
 	}
 }
