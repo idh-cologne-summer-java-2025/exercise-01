04839216fc3244266d092ef990330bf0385aebaa
chihayabu <chihayabu@proton.me>
2025-04-23 10:05:16 +0200

    Fehleranalyse
    einige Ideen die mir so ein Fehlen des 'boot class path' aufzeigt.

--- a/A.java
+++ a/A.java
@@ -1,22 +1,38 @@
+// mögliche Fehlerbehebung, findet allerding keinen boot class path (VSC)
 package a;
 
 import java.util.Random;
 
+
 public class A {
-	public static void (String args) {
-		int s = "Hello World!";
+	// 	fehlende Deklaration der Methode
+	//	public static void (String args) 
+	public static void main(String[] args)
+	{	
+		// da Random nirgends im Code festgelegt fehlt z.B.:
+		Random random = new Random();
+		// int s = "Hello World!";
+		// Hello World! ist ein string kein int Wert wird aber später als solcher genutzt
+		int s = 0;		
+		// selbige ggf auch für c = X, jedoch wird c nicht weiter genutzt
 		char c = 'X';
-		int[] numberList = int[15];
-		for (int i; i < numberList.length; i++) {
+		// Array startet tatsächlich mit new int
+		int[] numberList = new int[15];
+		// Schleife muss z.B. bei 0 starten
+		for (int i = 0; i < numberList.length; i++) {
 			numberList[i] = random.nextInt(100);
 		}
-		
-		sum = 0;
-		for (int i = 0; i < numberList.length; c++) {
+
+		// sum nicht als Variable int deklariert
+		int sum = 0;
+		// plötzlich soll c also X inkrementiert werden?
+		for (int i = 0; i < numberList.length; i++) {
 			sum += numberList[i];
 		}
-		if sum > 750 {
-			println(s);
+		
+		// Schliefenklammern
+		if (sum > 750) {
+			// unvollständige Ausgabebefehl, ggf Verwendung von c hier
+			System.out.println(c);
 		}
 	}
-}
