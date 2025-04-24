599aa09a8d295d36db5bee85352192b2c8c47b5a
HeadEmptyNoThoughts <mlemart27@gmail.com>
2025-04-24 16:16:38 +0200

    Fertige Aufgabe 1

--- a/A.java
+++ a/A.java
@@ -3,20 +3,21 @@ package a;
 import java.util.Random;
 
 public class A {
-	public static void (String args) {
-		int s = "Hello World!";
-		char c = 'X';
-		int[] numberList = int[15];
-		for (int i; i < numberList.length; i++) {
+	public static void main(String[] args) {
+		String s = "Hello World!";
+		//char c = 'X';  not necessary in my opinion, because it's not used!
+		Random random = new Random (); //we need this to create random numbers!
+		int[] numberList = new int[15];
+		for (int i=0; i < numberList.length; i++) {
 			numberList[i] = random.nextInt(100);
 		}
 		
-		sum = 0;
-		for (int i = 0; i < numberList.length; c++) {
+		int sum = 0;
+		for (int i = 0; i < numberList.length; i++) {
 			sum += numberList[i];
 		}
-		if sum > 750 {
-			println(s);
+		if (sum > 750) {
+			System.out.println(s);
 		}
 	}
 }
