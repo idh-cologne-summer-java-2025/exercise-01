6873bb4fd96340bd4871aa7af19f65d2851a3af9
efpis <efpis@pc.fritz.box>
2025-04-26 11:42:52 +0200

    exercise-01 Elias pischke

--- b/ATM.java
+++ b/ATM.java
@@ -1,19 +1,21 @@
 package b;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ATM {
-	
+	int accountBalance = 100;
+
 	/**
-	 * Main command loop of the ATM
-	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
-	 * which actually does the calculation and produces money.
-	 * If the user enters anything else than an integer number, the loop breaks 
-	 * and the program exists
+	 * Main command loop of the ATM Asks the user to enter a number, and passes this
+	 * number to the function cashout(...) which actually does the calculation and
+	 * produces money. If the user enters anything else than an integer number, the
+	 * loop breaks and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
+		while (true) {
 			try {
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
@@ -23,11 +25,17 @@ public class ATM {
 			}
 		}
 	}
-	
+
 	public void cashout(int amount) {
-		
+		if (amount < accountBalance) {
+			accountBalance = accountBalance - amount;
+			System.out.println("Ok, here is your money, enjoy!");
+		} else {
+			System.out.println("Sorry, not enough money in the bank.");
+		}
+
 	};
-	
+
 	/**
 	 * Launches the ATM
 	 */
@@ -35,4 +43,5 @@ public class ATM {
 		ATM atm = new ATM();
 		atm.run();
 	};
-};
+
+}
